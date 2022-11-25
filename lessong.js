75161700f23723287eb754e1d864e1357ec33d40
Ayodele <ayodeleaderibigbe2000@gmail.com>
2022-11-25 08:00:52 +0400

    thirteenth commit

---
+++ lessong.js
@@ -0,0 +1,82 @@
+let lesson =[
+    {
+        "id": 1,
+        "title": "Math",
+        "location": "Block 16",
+        "price": 1000,
+        "availability": 5,
+        "imgURL" : "images/maths.png"
+    },
+    {
+        "id": 2,
+        "title": "English",
+        "location": "Block 17",
+        "price": 3000,
+        "availability": 5,
+        "imgURL" : "images/english.png"
+    },
+    {
+        "id": 3,
+        "title": "French",
+        "location": "Ghana Campus",
+        "price": 4000,
+        "availability": 5,
+        "imgURL" : "images/french.png"
+    },
+    {
+        "id": 4,
+        "title": "Music",
+        "location": "America",
+        "price": 5000,
+        "availability": 5,
+        "imgURL" : "images/musici.png"
+    },
+    {
+        "id": 5,
+        "title": "Spanish",
+        "location": "Spain",
+        "price": 6000,
+        "availability": 5,
+        "imgURL" : "images/spanish.png"
+    },
+    {
+        "id": 6,
+        "title": "Knitting",
+        "location": "America",
+        "price": 9000,
+        "availability": 5,
+        "imgURL" : "images/knitting.png"
+    },
+    {
+        "id": 7,
+        "title": "Bible Study",
+        "location": "Church",
+        "price": 13000,
+        "availability": 5,
+        "imgURL" : "images/bible.jpg"
+    },
+    {
+        "id": 8,
+        "title": "Dancing",
+        "location": "Block 17",
+        "price": 14000,
+        "availability": 5,
+        "imgURL" : "images/dancing.jpg"
+    },
+    {
+        "id": 9,
+        "title": "Boxing",
+        "location": "London",
+        "price": 15000,
+        "availability": 5,
+        "imgURL" : "images/boxing.png"
+    },
+    {
+        "id": 10,
+        "title": "Maths",
+        "location": "Oxford",
+        "price": 20000,
+        "availability": 5,
+        "imgURL" : "images/maths.png"
+    }
+  ]
